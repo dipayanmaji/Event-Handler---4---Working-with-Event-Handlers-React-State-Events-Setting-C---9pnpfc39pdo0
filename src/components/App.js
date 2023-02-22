e2b44b8f3f613d150b899506ee631b21f8a7f9eb
Dipayan Maji <dipayanmaji1112@gmail.com>
2023-02-23 00:40:28 +0530

    final sumbit with proper meaningful answer

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
   const handleDoubleClick = (event) =>{
-    // if(event.detail === 1){
-    //   console.log("I was not double clicked");
-    // }else if(event.detail ===2){
+    if(event.detail === 1){
+      console.log("I was not double clicked");
+    }else if(event.detail ===2){
       console.log("I was double clicked");
-    // }
+    }
+  }
+
+  /*const handleDoubleClick = (event) =>{
+      console.log("I was double clicked");
   }
   const handleSingleClick = (event)=>{
       console.log("I was not double clicked");
-  }
+  }*/
   
   return (
     <div id="main">
-      <button onClick={handleSingleClick} onDoubleClick={handleDoubleClick} id="dblclick-btn">Double click me</button>
+      {/* <button onClick={handleSingleClick} onDoubleClick={handleDoubleClick} id="dblclick-btn">Double click me</button> */}
+      <button onClick={handleDoubleClick} id="dblclick-btn">Double click me</button>
     </div>
   )
 }
