708c70c2e4fe68d9cf16cb48258c2efa48fd3f67
Dipayan Maji <dipayanmaji1112@gmail.com>
2023-02-23 00:33:33 +0530

    testing some logic

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-    if(event.detail === 1){
-      console.log("I was not double clicked");
-    }else if(event.detail ===2){
+    // if(event.detail === 1){
+    //   console.log("I was not double clicked");
+    // }else if(event.detail ===2){
       console.log("I was double clicked");
-    }
+    // }
+  }
+  const handleSingleClick = (event)=>{
+      console.log("I was not double clicked");
   }
   
   return (
     <div id="main">
-      <button onClick={handleDoubleClick} id="dblclick-btn">Double click me</button>
+      <button onClick={handleSingleClick} onDoubleClick={handleDoubleClick} id="dblclick-btn">Double click me</button>
     </div>
   )
 }
